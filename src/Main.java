a0dc3bf3cebc913470c1ee6d50b57203b1529750
avm0402045632 <123787974+avm0402045632@users.noreply.github.com>
2023-02-13 22:20:35 -0600

    Registro de dispositivo

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import org.vmtz.metodos.Argumentos01;
 import org.vmtz.metodos.Clase01;
 import org.vmtz.metodos.Clase02;
 
 public class Main {
     public static void main(String[] args) {
-        traerMetododeClase02();
+        //traerMetododeClase02();
        /*
         Clase01 clase01 = new Clase01();
         clase01.saludar();
@@ -19,6 +20,11 @@ public class Main {
     public static void traerMetododeClase02(){
         Clase02 clase02 = new Clase02();
         clase02.ejecutar();
+
+    }
+    public static void traerMetodoArgumentos01(){
+        Argumentos01 argumentos01 = new Argumentos01();
+        argumentos01.registraTuDispositivo();
 
     }
 }
